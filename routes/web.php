c2c54196bb47641adb13578a2dd5b87e6eb971df
mgh <mostafaghandam111@gmail.com>
2025-12-31 16:14:44 +0100

    Implement authentication routes, views, and tests

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\CommandesController;
 use App\Http\Controllers\FacturesController;
 
-use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
-    return view('Acceuil');
+    return view('welcome');
 });
 
-#__________________Ressources_____________________
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 
 Route::resource('clients', ClientsController::class);
 Route::resource('commandes', CommandesController::class);
 Route::resource('factures', FacturesController::class);
+
+require __DIR__.'/auth.php';
